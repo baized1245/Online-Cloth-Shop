8875b2d82f0117eb1f98710838f4937709b8ef53
unknown <you@example.com>
2024-01-02 13:41:23 +0600

    Add RelatedProducts component

--- frontend/src/Pages/Product.jsx
+++ frontend/src/Pages/Product.jsx
@@ -5,6 +5,7 @@ import { useParams } from 'react-router-dom';
 import Breadcrum from '../Components/Breadcrum/Breadcrum';
 import ProductDisplay from '../Components/ProductDisplay/ProductDisplay';
 import DescriptionBox from '../Components/DescriptionBox/DescriptionBox';
+import RelatedProducts from '../Components/RelatedProducts/RelatedProducts';
 
 const Product = () => {
     const { all_product } = useContext(ShopContext);
@@ -15,7 +16,8 @@ const Product = () => {
         <div>
            <Breadcrum product={product}/> 
            <ProductDisplay product={product}/>
-           <DescriptionBox/> 
+           <DescriptionBox/>
+           <RelatedProducts/> 
         </div>
     );
 };
